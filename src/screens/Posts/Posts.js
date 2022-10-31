563371ed2124f2c48513ff68c96c9251066d2f18
tufronicolas <ntufro@udesa.edu.ar>
2022-10-31 14:45:36 -0300

    camara

--- src/screens/Posts/Posts.js
+++ src/screens/Posts/Posts.js
@@ -1,16 +1,19 @@
 import { Text, View, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
 import React, { Component } from 'react'
 import {auth, db} from '../../firebase/config'
-
+import Camara from '../../components/Camara/Camara'
 
 class Posts extends Component {
 
     constructor(){
         super()
         this.state={
-            description:''
+            description:'',
+            mostrarCamara:true,
+            fotoUrl:''
         }
     }
+
 
     enviarPost(description){
         db.collection('posts').add({
@@ -25,10 +28,21 @@ class Posts extends Component {
 
     }
 
-
+    cuandoSubaLaImagen(url){
+        this.setState({
+            mostrarCamara:false,
+            fotoUrl: url
+        })
+    }
 
     render() {
         return (
+            <View style = {styles.container}>
+            {
+                this.state.mostrarCamara ?
+                <Camara
+                cuandoSubaLaImagen = {(url)=> this.cuandoSubaLaImagen(url)}
+                /> :
         <View>
             <TextInput
                 keyboardType='default'
@@ -44,7 +58,8 @@ class Posts extends Component {
             </TouchableOpacity>
         </View>
 
-
+            }
+            </View>
         )
     }
 }
@@ -53,7 +68,10 @@ const styles = StyleSheet.create({
     input:{
         borderWidth:1,
         height:48
-    }
+    },
+    container:{
+        flex:1
+    }, 
 })
 
 export default Posts
