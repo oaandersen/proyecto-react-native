fa9f17a9c54582aeb4a8f19efbb27d5f74c02d49
Facundo Bory <facubory@MacBook-Pro-de-Facundo-2.local>
2022-11-24 20:36:32 -0300

    FriendProfile avances

--- src/screens/FriendProfile/FriendProfile.js
+++ src/screens/FriendProfile/FriendProfile.js
@@ -1,16 +1,69 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { View, Text} from 'react-native';
+import {FlatList } from 'react-native-web';
+import { db } from '../../firebase/config';
+import Post from '../../components/Post/Post';
 
 export default class FriendProfile extends Component {
-    constructor(props){
-        super(props)
+  constructor() {
+    super()
+    this.state = {
+      friendPosts: [],
+      user: [],
     }
+  }
+
+
+  componentDidMount() {
+    console.log(this.props);
+    db.collection('posts').where('owner', '==', this.props.route.params.email)
+      .onSnapshot(docs => {
+        let posts = []
+        docs.forEach(doc => { posts.push({
+            id: doc.id,
+            data: doc.data()
+          })
+          this.setState({
+            friendPosts: posts,
+          }, () => console.log(this.state))
+        })
+      })
+    db.collection('users').where('email', '==', this.props.route.params.email).onSnapshot(docs => {
+      let user = []
+      docs.forEach(doc => {
+        user.push({
+          id: doc.id,
+          data: doc.data()
+        })
+        this.setState({
+          user: user[0],
+          username: user[0].data.userName,
+          bio: user[0].data.miniBio,
+          email: user[0].data.email
+        }, () => {
+          console.log(this.state)
+        })
+      })
+    })
+  }
   render() {
-    console.log(this.props)
     return (
       <View>
-        <Text>FriendProfile</Text>
+        <Text>My Profile</Text>
+        <View>
+          <Text>{this.state.username}</Text>
+          <Text>{this.state.email}</Text>
+          <Text>Posts: {this.state.friendPosts.length}</Text>
+          <Text>{this.state.bio}</Text>
+        </View>
+        <FlatList
+          data={this.state.friendPosts}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item }) => <Post navigation={this.props.navigation} id={item.id} data={item.data} />}
+        />
       </View>
     )
   }
+
 }
+
